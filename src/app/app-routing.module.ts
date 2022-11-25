feeee4b56b69245ddf1e2059681ca5efe9a6703d
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-25 15:25:45 +0530

    add post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPostComponent } from './add-post/add-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:AddPostComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
