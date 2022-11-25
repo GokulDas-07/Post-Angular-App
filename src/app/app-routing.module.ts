064ed8ad1e26c1e7d17adc74f21c1f1aa42d81ad
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-25 15:33:30 +0530

    view post

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddPostComponent } from './add-post/add-post.component';
+import { ViewPostComponent } from './view-post/view-post.component';
 
 const routes: Routes = [
-  {path:"",component:AddPostComponent}
+  {path:"",component:AddPostComponent},
+  {path:"view",component:ViewPostComponent}
 ];
 
 @NgModule({
